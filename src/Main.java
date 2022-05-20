3563490a9822e15d7099f79c0143d11f16901ba4
anastasiynovaya <aefremova3014@gmail.com>
2022-05-20 16:03:15 +0500

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int money = 100;
-        int cost = 1100;
-        int bonus = (cost / 100);
+        int money = 100; //количество денег на текущем счете
+        int cost = 1100; //сумма пополнения
+        int bonus = (cost / 100); //количество бонусных рублей
         int sum = money + cost + bonus;
         int sum2 = money + cost;
 
         if (cost > 1000) {
-            System.out.println("Итоговый счёт и количество бонусных рублей " + sum);
+            System.out.println("Итоговый счёт " + sum + " количество бонусных рублей " + bonus);
         } else {
             System.out.println("Бонусов нет " + "сумма пополнения " + sum2);
         }
